edf390be2b0d0ae438e2694c12ffa5e7649e2f27
phuctran121 <phuc12104@gmail.com>
2025-06-07 12:39:16 +0700

    feat(Contact): Adding working send contact message

--- components/sections/ContactSection.tsx
+++ components/sections/ContactSection.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState, useRef } from "react";
 import { motion, MotionConfig } from "motion/react";
 import {
   FaGithub,
@@ -39,6 +39,35 @@ const socialIcons = [
 ];
 
 export default function ContactSection() {
+  const [successMessage, setSuccessMessage] = useState("");
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
+    const formData = new FormData(e.target as HTMLFormElement);
+
+    formData.append("access_key", "f25cd664-5a00-4ae6-be62-455fbaabc415");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: json,
+    });
+    const result = await response.json();
+    if (result.success) {
+      setSuccessMessage("Your message has been sent successfully!");
+      form.reset();
+      setTimeout(() => setSuccessMessage(""), 3000);
+    }
+  };
+
   return (
     <section className="min-h-screen flex flex-col items-center justify-center text-white px-4 py-24 md:py-12 ">
       <AnimatedCornerBorder number="04" width="1248px" height="672px" />
@@ -81,6 +110,8 @@ export default function ContactSection() {
 
         {/* Form */}
         <motion.form
+          ref={formRef}
+          onSubmit={handleSubmit}
           initial={{ opacity: 0, y: 40, filter: "blur(3px)" }}
           animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
           transition={{ duration: 1, delay: 0.5 }}
@@ -88,15 +119,18 @@ export default function ContactSection() {
         >
           <input
             type="text"
+            name="name"
             placeholder="Name"
             className="bg-transparent border w-full border-white/40 rounded px-4 py-4 placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-white"
           />
           <input
             type="email"
+            name="email"
             placeholder="Email"
             className="bg-transparent border w-full border-white/40 rounded px-4 py-4 placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-white"
           />
           <textarea
+            name="message"
             rows={6}
             placeholder="Enter your message"
             className="bg-transparent border w-full border-white/40 rounded px-4 py-4 placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-white"
@@ -107,6 +141,12 @@ export default function ContactSection() {
           >
             Send Message
           </button>
+
+          {successMessage && (
+            <p className="text-green-400 w-full text-center text-sm mt-4 absolute bottom-16 left-1/2 transform -translate-x-1/2">
+              {successMessage}
+            </p>
+          )}
         </motion.form>
       </div>
     </section>
